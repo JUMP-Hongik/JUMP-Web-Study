ef5195226893a932384bbb7917a4aeff5b335156
younggyo <lee4519698@hanmail.net>
2023-07-22 23:41:48 +0900

    Feat : ref DOM에 이름 달기

--- YoungGyo-00/hello-react/src/App.js
+++ YoungGyo-00/hello-react/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import CEventPractice from './CEventPractice';
-import FEventPractice from './FEventPractice';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return (
-    <>
-      <CEventPractice />
-      <FEventPractice />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
