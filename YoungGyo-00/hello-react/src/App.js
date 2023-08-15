47428b015a674ff0f4ff2193508a9ee61a976c8b
younggyo <lee4519698@hanmail.net>
2023-08-16 02:38:07 +0900

    Feat : 컴포넌트 반복(추가, 제거) 기능

--- YoungGyo-00/hello-react/src/App.js
+++ YoungGyo-00/hello-react/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import ValidationSample from './sample/ValidationSample';
 import ScrollBox from './sample/ScrollBox';
+import IterationSample from './sample/IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
--- YoungGyo-00/hello-react/src/sample/IterationSample.js
+++ YoungGyo-00/hello-react/src/sample/IterationSample.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음'];
-  const nameList = names.map((name) => <li>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(3);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setInputText('');
+    setNames(nextNames);
+  };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
