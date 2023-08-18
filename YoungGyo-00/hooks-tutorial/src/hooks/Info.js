af26a14fb4cbfc1a94542093485196b226b200a4
younggyo <lee4519698@hanmail.net>
2023-08-18 23:37:22 +0900

    Feat : useReducer 인풋 상태 관리

--- YoungGyo-00/hooks-tutorial/src/hooks/Info.js
+++ YoungGyo-00/hooks-tutorial/src/hooks/Info.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+};
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickName] = useState("");
-  useEffect(() => {
-    console.log("렌더링 완료");
-    return () => {
-      console.log("clean");
-      console.log(name);
-    };
-  }, [name]);
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
 
-  const onChangeNickName = (e) => {
-    setNickName(e.target.value);
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickName} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
